1292c68ee1a0f86e74a41d4f0fffb5e1ea2d6c8e
linusansas <linusansas@gmail.com>
2024-04-05 16:09:44 +0200

    Fix indentation in page6.tsx

--- src/pages/page6.tsx
+++ src/pages/page6.tsx
@@ -1,3 +1,7 @@
+//Indentation problem:
+//Regel: indent
+//Beskrivning: Kontrollerar att kod är korrekt indenterad för att öka läsbarheten.
+
 function Exempel6() {
    return (
       <h1 className="flex justify-center font-thin mb-2 mt-16 md:mt-0 md:ml-0">
@@ -7,3 +11,16 @@ function Exempel6() {
 }
 
 export default Exempel6;
+
+//Lösning
+/*
+function Exempel6() {
+   return (
+      <h1 className="flex justify-center font-thin mb-2 mt-16 md:mt-0 md:ml-0">
+         6
+      </h1>
+   );
+}
+
+export default Exempel6;
+*/
